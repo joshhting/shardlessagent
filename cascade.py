115841ab95d2edab97161f7b01cb558a435c759f
Josh Ting <joshting25@gmail.com>
2024-03-03 00:12:10 -0500

    Check for a/an

--- cascade.py
+++ cascade.py
@@ -4,9 +4,16 @@ def cascade(arg: str):
     f = open("lines.txt", "r")
     templateList = f.readlines()
     template = random.choice(templateList)
-    # template = templateList[33] # uncomment for specific template
+    # template = templateList[3] # uncomment for specific template
+
+    # check to capitalize arg
     if template[0] == '+':
         arg = arg.upper()
         template = template[1:]
+
+    # check for a/an
+    article = "an" if ((0x208222 >> (ord(arg[0]) & 0x1f)) & 1) else "a"
+    template = template.replace("a(n)", article)
+
     joke = template.replace('#', arg)
     return joke

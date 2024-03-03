0a6c3a96c6d21f1c3bdb1de1c98c58d13b430fcd
Josh Ting <joshting25@gmail.com>
2024-03-02 23:54:54 -0500

    Capitalize context

--- cascade.py
+++ cascade.py
@@ -2,7 +2,11 @@ import random
 
 def cascade(arg: str):
     f = open("lines.txt", "r")
-    jokes = f.readlines()
-    joke = random.choice(jokes).replace('#', arg)
-    # joke = jokes[3].replace('#', arg) # uncomment for pepsi
+    templateList = f.readlines()
+    template = random.choice(templateList)
+    # template = templateList[33] # uncomment for specific template
+    if template[0] == '+':
+        arg = arg.upper()
+        template = template[1:]
+    joke = template.replace('#', arg)
     return joke
